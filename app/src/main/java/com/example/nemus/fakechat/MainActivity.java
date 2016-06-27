126f0f48f2765621de23bea3bf41867f3547ccb2
yeansang ahn <ysan1991@gmail.com>
2016-06-27 16:47:40 +0900

    add input null exception

--- app/src/main/java/com/example/nemus/fakechat/MainActivity.java
+++ app/src/main/java/com/example/nemus/fakechat/MainActivity.java
@@ -4,18 +4,20 @@ import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.Vector;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText memoToUp = null;
-    ListView up = null;
+    EditText inputText = null;
+    ListView screen = null;
     DBConnect dbConnect;
     ArrayAdapter<String> adapter;
     int last=0;
@@ -25,15 +27,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         //초기화
+        //database 연결
         dbConnect = new DBConnect(getApplicationContext(), "chat.db",null,1);
-        memoToUp = (EditText) findViewById(R.id.editText);
-        up = (ListView) findViewById(R.id.listView);
+        //입력창, 리스트, 리스트 어댑터 생성
+        inputText = (EditText) findViewById(R.id.editText);
+        screen = (ListView) findViewById(R.id.listView);
         adapter= new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1);
 
+        //초기값 받아오기
         Vector<String> preset = dbConnect.getAll();
-
-
         if(preset != null) {
             for (int i = 0; i < preset.size(); i++) {
                 adapter.add(preset.elementAt(i));
@@ -41,22 +45,36 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        up.requestFocusFromTouch();
-        up.setAdapter(adapter);
+        //리스트 내리기 위해 설정
+        screen.requestFocusFromTouch();
+        //리스트 어댑터 setting
+        screen.setAdapter(adapter);
     }
 
 
     public void onClick(View v) {
         if (v.getId() == R.id.button) {
-            Editable memo = memoToUp.getText();
-            memoToUp.setText("");
-            adapter.add(memo.toString());
 
-            dbConnect.input(memo.toString());
+            //텍스트 받아오기
+            Editable memo = inputText.getText();
+            inputText.setText("");
 
-            adapter.notifyDataSetChanged();
-            last++;
-            up.setSelection(last);
+            //텍스트가 공백이 아닐때
+            if(!memo.toString().equals("")) {
+                //리스트에 추가
+                adapter.add(memo.toString());
+                //리스트 갱신
+                adapter.notifyDataSetChanged();
+                //db에 추가
+                dbConnect.input(memo.toString());
+                //화면 내리기
+                last++;
+                screen.setSelection(last);
+            }else{
+                Toast toast = Toast.makeText(getApplicationContext(),"Input is NULL!!", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+            }
        }
     }
 
